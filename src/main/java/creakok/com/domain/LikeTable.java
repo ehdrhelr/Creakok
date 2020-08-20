5fb8df29aa07c43289065ef546c26419bf7a1f08
daumhch <66451256+daumhch@users.noreply.github.com>
2020-08-20 17:37:03 +0900

    hcbae LikeTable에서 seq 컬럼을 삭제. 그냥 막 쓰고 막 지우는걸로.그리고 불필요한 bak 파일도 삭제.

--- src/main/java/creakok/com/domain/LikeTable.java
+++ src/main/java/creakok/com/domain/LikeTable.java
@@ -8,7 +8,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class LikeTable {
-	private long like_index;
 	private long like_type_code;
 	private long like_content_index;
 	private String like_member_email;
