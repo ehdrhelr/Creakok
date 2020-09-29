e1ce4309864b2ebcab21e4decb93b8349944a1b8
daumhch <daumhch@naver.com>
2020-09-30 01:05:37 +0900

    hcbae mariaDB보완-서브쿼리에서 order by가 먹지 않기 때문에 desc 이후에 limit을 걸어서 페이징을 보완
    hikari 설정 되살림.

--- src/main/java/creakok/com/web/RootContextConfig.java
+++ src/main/java/creakok/com/web/RootContextConfig.java
@@ -2,9 +2,11 @@ package creakok.com.web;
 
 import javax.sql.DataSource;
 
-import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 import creakok.com.filesetting.Path;
 
@@ -13,6 +15,7 @@ public class RootContextConfig {
 
 	@Bean(destroyMethod = "close")
 	public DataSource dataSource() {
+		/*
 		BasicDataSource dataSource = new BasicDataSource();
 		dataSource.setDriverClassName("org.mariadb.jdbc.Driver");
 		dataSource.setUrl("jdbc:mariadb://localhost:3306/creakok");
@@ -24,7 +27,22 @@ public class RootContextConfig {
 			dataSource.setUsername("root");
 			dataSource.setPassword("java0420");
 		}
+		*/
 		
+		HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setDriverClassName("org.mariadb.jdbc.Driver");
+		hikariConfig.setJdbcUrl("jdbc:mariadb://localhost:3306/creakok");
+		
+		if(Path.STORE_PATH == Path.CAFE24_PATH) {
+			hikariConfig.setUsername("creakok");
+			hikariConfig.setPassword("creakokpass123");			
+		} else {
+			hikariConfig.setUsername("root");
+			hikariConfig.setPassword("java0420");
+		}
+
+		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+
 		return dataSource;
 	}
 }
