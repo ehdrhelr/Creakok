ff7beaa5ea2206bb1b63662c2393d5d4b952c24e
Shion <ehdrhelr@gmail.com>
2020-10-05 13:01:40 +0900

    feat : Fix Path.java

--- src/main/java/creakok/com/filesetting/Path.java
+++ src/main/java/creakok/com/filesetting/Path.java
@@ -13,13 +13,8 @@ public class Path {
 	public static final String CAFE24_PATH = "/creakok/tomcat/webapps/ROOT/resources";
 	
 	
-	//위 리스트 중에 하나로 STORE_PATH만 바꾸면 된다.
-<<<<<<< HEAD
 	public static final String STORE_PATH = shion_path;
-=======
-	public static final String STORE_PATH = jihee_path;
->>>>>>> branch 'master_maria' of https://github.com/daumhch/Creakok.git
-	
+
 	
 	public static final String FUNDING_PATH = "/img/funding/";
 	public static final String GOODS_PATH = "/img/goods/";
