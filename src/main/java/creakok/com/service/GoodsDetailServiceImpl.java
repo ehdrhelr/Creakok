38abe62a77a156c9953acf3cd4e9829b8580d578
bit <bit@DESKTOP-L8OVE23>
2020-10-07 20:14:35 +0900

    커밋앤푸시ㅣ

--- src/main/java/creakok/com/service/GoodsDetailServiceImpl.java
+++ src/main/java/creakok/com/service/GoodsDetailServiceImpl.java
@@ -16,6 +16,8 @@ public class GoodsDetailServiceImpl implements GoodsDetailService {
 	@Override
 	public Goods getGoodsDetail(long goods_index) {
 		Goods one_goods = goods_detail_mapper.getGoodsDetail(goods_index);
+		log.info("아니이게 말이 되냐고!!!!!!!!!!!!!!!!!!!!!!!!!!!: "+goods_index);
+		log.info("아니이게 말이 되냐고!!!!!!!!!!!!!!!!!!!!!!!!!!!: "+one_goods);
 		return one_goods;
 	}
 
